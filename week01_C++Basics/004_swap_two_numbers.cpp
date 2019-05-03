434af00279ccadad2b1311be112ef081694f9d44
phatom0122 <wilko97@naver.com>
2019-05-03 23:15:46 +0900

    Create 004_swap_two_numbers.cpp

---
+++ week01_C++Basics/004_swap_two_numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int  A, B, C;
+
+	cout << "Please enter two integar values: "<<"\n";
+	cout << "A : "; cin >> A;
+	cout <<"B : "; cin >> B;
+
+	C = B;
+	B = A;
+	A = C;
+
+	cout << "Value of A is : " << A << "\n";
+	cout << "Value of B is : " << B << "\n";
+
+
+
+
+
+
+}
