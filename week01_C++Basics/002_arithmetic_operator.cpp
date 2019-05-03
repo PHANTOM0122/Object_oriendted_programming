70b36844dc16f96b481b740a2a0352b1e515caac
phatom0122 <wilko97@naver.com>
2019-05-03 23:15:41 +0900

    Create 002_arithmetic_operator.cpp

---
+++ week01_C++Basics/002_arithmetic_operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int x, y;
+	cout << "Please enter two integar values: ";
+	cin >> x >> y;
+	cout << x << "+" << y << "=" << x + y << "\n";
+	cout << x << "-" << y << "=" << x - y << "\n";
+	cout << x << "*" << y << "=" << x * y << "\n";
+	cout << x << "/" << y << "=" << x / y << "\n";
+	cout << x << "%" << y << "=" << x % y << "\n";
+}
