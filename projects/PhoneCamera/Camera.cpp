281b8ee67c1daf9a1c2462a4949de47db1241b50
phatom0122 <wilko97@naver.com>
2019-05-17 14:28:04 +0900

    Create Camera.cpp

---
+++ projects/PhoneCamera/Camera.cpp
@@ -0,0 +1,14 @@
+#include "Camera.h"
+#pragma once
+#include <iostream>
+using namespace std;
+
+void Camera::Photograph()
+{
+	cout << "Take a photo..." << endl;
+}
+
+void Camera::Zoom(int zoom)
+{
+	cout << "Zoom..." << zoom << endl;
+}
